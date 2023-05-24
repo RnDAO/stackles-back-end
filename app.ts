ba5b155df029d2a5a0bdda3b98343aed2cca62d1
idivanshu <divanshuprajapat2002@gmail.com>
2023-05-24 13:23:23 +0530

    auth

--- app.ts
+++ app.ts
@@ -13,8 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Replace with the origin of your frontend application
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Replace with the origin of your frontend application
-
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials (cookies, headers) to be sent with requests
@@ -24,7 +22,8 @@ app.use(cors(
   {
     origin: 'http://localhost:3000',
     methods: 'GET, POST, PUT, DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
+    allowedHeaders: ['Content-Type, Authorization'],
+    // header: 'Content-Type, Authorization',
     credentials: true // Allow credentials (cookies, headers) to be sent with requests
   }
 ));
