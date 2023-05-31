6f7b75c37cc12d37ff55d9fb5e4d5b713c80f0ce
idivanshu <divanshuprajapat2002@gmail.com>
2023-05-31 21:10:25 +0530

    origin changed4

--- Middlewares/auth.ts
+++ Middlewares/auth.ts
@@ -20,14 +20,16 @@ const auth = async (req : any, res: any, next: any)=>{
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials (cookies, headers) to be sent with requests
       // console.log(req.cookies);
-      const token = req.cookies.token;
-      // console.log(token);
-      if(!token){
-          res.status(401).json({
-              message: 'Unauthorized - no token'
-          })
+      const authorizationHeader = req.headers.authorization;
+    
+      if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Unauthorized - no token' });
       }
+      
+     
+      
       else{
+        const token = authorizationHeader.slice(7); // Remove 'Bearer ' prefix
         const decoded : any = jwt.verify(token, JWT_SECRET);
         const email: string   = decoded.email;
         // console.log(decoded);
